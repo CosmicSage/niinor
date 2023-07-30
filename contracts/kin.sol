8a7a6e2b25716bcc2d43f143c4338af3fec07fef
CosmicSage <prabhakarmishra9@yahoo.com>
2023-07-30 18:14:58 +0530

    New Contract

--- contracts/kin.sol
+++ contracts/kin.sol
@@ -6,9 +6,6 @@ import "hardhat/console.sol";
 
 contract Kin {
     string public something = "hahaha";
-    bytes public randombytes;
-    address public whostheking;
-    address public whosthis;
 
     function validateTest(address payable _ethtar /*address to grab*/) external {
 
@@ -18,12 +15,6 @@ contract Kin {
        // .Call with value 0
       //  (bool result,) = address(_tar).call{value:0}("");
       //  !result;
-    }
-
-    function abiGenerator() public {
-      randombytes = abi.encodeWithSignature("cheating(address)", "0x8eF3D3ddF2587268CaF5be6B004a97D58cc3Dcc1");
-
-
     }
 
     function darkSubmit(address payable _instance, uint eth, string memory code) public {
@@ -40,22 +31,16 @@ contract Kin {
     function vcard(address payable _instance, address _player, uint eth, string memory code) public returns (bool) {
         _player;
         King instance = King(_instance);
-        (bool result,) = address(instance).call{value:0 ether}("");
+        (bool result,) = address(instance).call{value:eth, gas:1000000}("");
         console.log("card result : %s", result);
         (bool result1,) = _instance.call{value:eth, gas:1000000}("");
         console.log("card result1 : %s", result1);
 
-        // Letss findout who's the king
-        whostheking = instance._king();
-        whosthis = address(this);
         !result;
         return instance._king() != address(this);
   }
   
-  // not my work
-  function cheating(address _instance) payable public {
-    (bool result, bytes memory data) = _instance.call{value:msg.value}("");
-    if (!result) revert();
+  receive() external payable {
 
   }
 
